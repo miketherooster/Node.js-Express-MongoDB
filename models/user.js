6086a13943c948f945f70c00c2cb12dfa3475a3e
MikeTheRooster <miketherooster63@gmail.com>
2021-03-20 11:37:39 -0500

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -3,12 +3,19 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
     admin: {
         type: Boolean,
         default: false
     }
 });
-
 userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
