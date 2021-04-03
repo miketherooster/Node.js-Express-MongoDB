51565c4aa7e8c5b5e4668b9d00b9ab356a6e82a8
miketherooster <74082611+miketherooster@users.noreply.github.com>
2021-04-03 00:42:00 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '1662147703973418',
-        clientSecret: 'a18f9de15db2b81db920f0a506ae5f81'
-    }
-}
